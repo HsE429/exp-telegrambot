532ecac7831d816a4ad5e6496b6bec697b1079f3
p <esp9900k@gmail.com>
2026-01-25 19:07:51 +0900

    创建一个简单的reaction回调处理器

--- demo-reaction-bot/README.md
+++ demo-reaction-bot/README.md
@@ -3,3 +3,15 @@
 使用这个项目测试Telegram机器人的Reaction功能。
 
 在 python-telegram-bot ≥ v20.0 中，Telegram 把“消息表情反应”抽象成了 ReactionType，其中：`telegram.ReactionTypeEmoji`表示 普通 emoji 反应，比如 👍 👎 ❤️ 😂 等。
+
+监听`MessageReactionUpdated`，通过`update.message_reaction`来获取信息。
+
+```python
+reaction_update = update.message_reaction
+
+reaction_update.chat           # 群组
+reaction_update.message_id     # 被反应的消息 ID
+reaction_update.user           # 点反应的人
+reaction_update.new_reaction   # 新增的反应列表
+reaction_update.old_reaction   # 原来的反应列表
+```
--- demo-reaction-bot/main.py
+++ demo-reaction-bot/main.py
@@ -1,6 +1,6 @@
 import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram import Update, ReactionTypeEmoji
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageReactionHandler, MessageHandler, filters
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -10,10 +10,25 @@ logging.basicConfig(
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+async def handle_reaction(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    reaction_update = update.message_reaction
+
+    chat = reaction_update.chat
+    message_id = reaction_update.message_id
+    user = reaction_update.user
+    new_reaction = reaction_update.new_reaction
+
+    print(chat, user, message_id, new_reaction)
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token('8075682427:AAGlIlHOGit78OIUsgR_IHw9ySLuQN9zHF0').build()
     
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), echo))
+    application.add_handler(MessageReactionHandler(handle_reaction))
     
-    application.run_polling()
+    # 重要提示：添加 allowed_updates 参数。
+    application.run_polling(allowed_updates=['message_reaction'])
