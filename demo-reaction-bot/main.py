d506bb355f77257f401b7c1c9ed94a0ecc876c04
p <esp9900k@gmail.com>
2026-01-26 13:02:49 +0900

    移除无用的请求处理器

--- demo-reaction-bot/main.py
+++ demo-reaction-bot/main.py
@@ -10,9 +10,6 @@ logging.basicConfig(
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
 async def handle_reaction(update: Update, context: ContextTypes.DEFAULT_TYPE):
     ru = update.message_reaction
 
@@ -34,7 +31,6 @@ if __name__ == '__main__':
     application = ApplicationBuilder().token('8075682427:AAGlIlHOGit78OIUsgR_IHw9ySLuQN9zHF0').build()
     
     application.add_handler(CommandHandler('start', start))
-    application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), echo))
     application.add_handler(MessageReactionHandler(handle_reaction))
     
     # 重要提示：添加 allowed_updates 参数。
